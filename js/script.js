66452e55c12e924bd41891072b349a9cd75e34d2
Thanuja153 <thanujapavanisatti@gmail.com>
2023-02-18 21:55:00 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
 
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
 
-var addForm = document.getElementById('addForm');
-var employees = document.getElementById('employees');
+let addForm = document.getElementById('addForm');
+let employees = document.getElementById('employees');
 
 
 
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
 
-var count = 0;
-var employeeCount = document.getElementById("empCount");
+let count = 0;
+let employeeCount = document.getElementById("empCount");
 
 
 // ADD EMPLOYEE
@@ -23,24 +23,24 @@ addForm.addEventListener('submit', (e) => {
 
     // GET THE VALUES FROM THE TEXT BOXES
     
-    const id = document.getElementById('id').value;
-    const name = document.getElementById('name').value;
-    const extension = document.getElementById('extension').value;
-    const email = document.getElementById('email').value;
-    const department = document.getElementById('department').value;
+    let id = document.getElementById('id').value;
+    let name = document.getElementById('name').value;
+    let extension = document.getElementById('extension').value;
+    let email = document.getElementById('email').value;
+    let department = document.getElementById('department').value;
 
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
     
-     const newrow = employees.insertRow(-1);
+    let newrow = employees.insertRow(-1);
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
     
-    const idCell = newrow.insertCell(0);
-    const nameCell = newrow.insertCell(1);
-    const extensionCell = newrow.insertCell(2);
-    const emailCell = newrow.insertCell(3);
-    const departmentCell = newrow.insertCell(4);
-    const deleteCell = newrow.insertCell(5);
+    let idCell = newrow.insertCell(0);
+    let nameCell = newrow.insertCell(1);
+    let extensionCell = newrow.insertCell(2);
+    let emailCell = newrow.insertCell(3);
+    let departmentCell = newrow.insertCell(4);
+    let deleteCell = newrow.insertCell(5);
     
 
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
@@ -53,7 +53,7 @@ addForm.addEventListener('submit', (e) => {
 
     // CREATE THE DELETE BUTTON
     
-    const deleteButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
     deleteButton.className = 'btn btn-danger btn-sm delete';
     deleteButton.appendChild(document.createTextNode('X'));
     deleteCell.appendChild(deleteButton);
@@ -78,7 +78,7 @@ addForm.addEventListener('submit', (e) => {
 employees.addEventListener('click', (e) => {
     if (e.target.classList.contains('delete')) {
         if (confirm('Please confirm that you want to delete this employee from the system?')) {
-            const newrow  = e.target.parentNode.parentNode;
+            let newrow  = e.target.parentNode.parentNode;
             employees.deleteRow(newrow .rowIndex);
             count--;
             employeeCount.textContent = count;
